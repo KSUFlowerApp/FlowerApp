6ebc32b8308e9200273570f649407be03a7b4c19
Eric1728 <e.johnson@magna.com>
2016-09-19 15:20:56 -0500

    login authentication
    defined the login handler routes

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 app.set('view engine', 'ejs');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('public'));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -23,8 +27,19 @@ app.get('/login', function(req, res) {
   res.render('login');
 });
 
-app.post('/login', urlencodedParser, function(req,res) {
-	res.send(req.body);
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/loginSuccess',
+    failureRedirect: '/loginFailure'
+  })
+);
+
+app.get('/loginFailure', function(req, res, next) {
+  res.send('Failed to authenticate');
+});
+
+app.get('/loginSuccess', function(req, res, next) {
+  res.send('Successfully authenticated');
 });
 
 app.listen(3000, function () {
