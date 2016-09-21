06217bacc5cc87275f67e32303938d5d3ac68341
tlentz <tcl@ksu.edu>
2016-09-21 11:11:08 -0500

    random login stuff - doesn't work

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var app = express();
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mysql = require('mysql');
+
+// Init App
+var app = express();
 
 /*
 use your own login credentials for sql connection
 */
 var connection = mysql.createConnection({
   host     : 'mysql.cis.ksu.edu',
-  user     : '',
-  password : '',
+  user     : 'tcl',
+  password : 'flowerapp',
   database : 'proj_flowerapp'
 });
 
@@ -23,12 +30,67 @@ connection.connect(function(err) {
   }
 });
 
-app.set('view engine', 'ejs');
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.set('db',connection);
 
-app.use(express.static('public'));
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'ejs');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport Init
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
 
 app.get('/', function (req, res) {
   res.redirect('login');
