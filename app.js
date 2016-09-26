e6abc9450bca12355c609ad06d7b141356d6d666
tlentz <tcl@ksu.edu>
2016-09-26 11:07:05 -0500

    Working db connection

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ use your own login credentials for sql connection
 */
 var connection = mysql.createConnection({
   host     : 'mysql.cis.ksu.edu',
-  user     : 'tcl',
-  password : 'flowerapp',
+  user     : 'projusr_flower',
+  password : 'rosesarenotredKSU',
   database : 'proj_flowerapp'
 });
 
@@ -31,97 +31,6 @@ connection.connect(function(err) {
 });
 
 app.set('db',connection);
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-// View Engine
-app.set('views', path.join(__dirname, 'views'));
-app.engine('ejs', exphbs({defaultLayout:'layout'}));
-app.set('view engine', 'ejs');
-
-// BodyParser Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-// Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Express Session
-app.use(session({
-    secret: 'secret',
-    saveUninitialized: true,
-    resave: true
-}));
-
-// Passport Init
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Express Validator
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
-
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
-
-// Connect flash
-app.use(flash());
-
-// Global Vars
-app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  res.locals.user = req.user || null;
-  next();
-});
-
-app.use('/', routes);
-app.use('/users', users);
-
-app.get('/', function (req, res) {
-  res.redirect('login');
-});
-
-app.get('/home', function(req, res) {
-  res.render('home');
-});
-
-app.post('/home', urlencodedParser, function(req,res) {
-	res.send(req.body);
-});
-
-app.get('/login', function(req, res) {
-  res.render('login');
-});
-
-app.post('/login',
-  passport.authenticate('local', {
-    successRedirect: '/loginSuccess',
-    failureRedirect: '/loginFailure'
-  })
-);
-
-app.get('/loginFailure', function(req, res, next) {
-  res.send('Failed to authenticate');
-});
-
-app.get('/loginSuccess', function(req, res, next) {
-  res.send('Successfully authenticated');
-});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
