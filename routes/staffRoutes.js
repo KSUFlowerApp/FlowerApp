d750eb253c329fdf1524a8900988b14d17a06e5e
Eric1728 <eric.1728.kc@gmail.com>
2016-11-15 21:35:59 -0600

    finished customers add/edit
    fully functioning add and edit customer, refreshes page after

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -25,6 +25,9 @@ module.exports = function(app, passport, db) {
     });
   });
 
+  // =====================================
+  // STAFF - CUSTOMERS =======================
+  // =====================================
   app.get('/staff/customers', session.isLoggedIn, function(req,res) {
     customers.getCustomers(function(err, customers) {
       if(err) {
@@ -34,4 +37,64 @@ module.exports = function(app, passport, db) {
       }
     });
   });
+
+  app.post('/staff/customers', function(req, res) {
+		var id = req.body.customerID;
+		var firstName = undefined;
+		var lastName = undefined;
+		var phone = undefined;
+    var email = undefined;
+    var addr = undefined;
+    var city = undefined;
+    var state = undefined;
+		var query = undefined;
+
+    console.log(id);
+		if (typeof id === 'undefined' || !id) {
+			var firstName = req.body.addFirstName;
+      var lastName = req.body.addLastName;
+      var phone = req.body.addPhone;
+      var email = req.body.addEmail;
+      var addr = req.body.addAddr;
+      var city = req.body.addCity;
+      var state = req.body.addState;
+      var zip = req.body.addZip;
+			query = "INSERT INTO customers(firstName, lastName, phone, e_mail, street_addr, city, state, zip) " +
+							"VALUES('"+firstName+"', '"+lastName+"', '"+phone+"', '"+email+"', '"+addr+"', '"+city+"', '"+state+"', '"+zip+"')";
+		}
+		else {
+      var firstName = req.body.customerFirstName;
+      var lastName = req.body.customerLastName;
+      var phone = req.body.customerPhone;
+      var email = req.body.customerEmail;
+      var addr = req.body.customerStreetAddr;
+      var city = req.body.customerCity;
+      var state = req.body.customerState;
+      var zip = req.body.customerZip;
+			query = "UPDATE customers " +
+							"SET firstName = '"+firstName+"', " +
+							"lastName = '"+lastName+"', " +
+              "phone = '"+phone+"', " +
+              "e_mail = '"+email+"', " +
+              "street_addr = '"+addr+"', " +
+              "city = '"+city+"', " +
+              "state = '"+state+"', " +
+              "zip = '"+zip+"' " +
+							"WHERE id = "+id;
+		}
+    console.log(query + " " + id);
+		db.query(query, function(err, rows) {
+			var response = {
+				status: 200,
+				message: "Customer info updated successfully."
+			}
+			if(err) {
+				response = {
+					status: 500,
+					message: "Error updating customer info."
+				}
+			}
+			res.end(JSON.stringify(response));
+		});
+	});
 };
