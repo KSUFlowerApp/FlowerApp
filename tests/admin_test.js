d71a1663d79576912cdf94724443bf250ff72eb9
tlentz <tyler.cg.lentz@gmail.com>
2017-01-17 10:23:50 -0600

    get inventory types test

--- tests/admin_test.js
+++ tests/admin_test.js
@@ -2,7 +2,12 @@ var expect = require('chai').expect;
 var admin = require('../middleware/admin.js');
 
 describe("Admin Middleware Tests", function() {
-  it("Test 1", function() {
-    expect("").to.equal("");
+  it("Get Inventory Types", function() {
+    var inventoryTypes = null;
+    admin.getInventoryTypes(function(err, rows) {
+      expect(err).to.equal(null);
+      inventoryTypes = rows;
+    });
+    expect(inventoryTypes).to.not.equal(null);
   });
 });
