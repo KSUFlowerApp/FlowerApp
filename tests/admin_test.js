2f63e8b95604b24c9fa3c8a2a048b5c85c618d08
tlentz <tyler.cg.lentz@gmail.com>
2017-01-17 10:00:07 -0600

    Added test modules and tests folder

---
+++ tests/admin_test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var admin = require('../middleware/admin.js');
+
+describe("Admin Middleware Tests", function() {
+  it("Test 1", function() {
+    expect("").to.equal("");
+  });
+});
