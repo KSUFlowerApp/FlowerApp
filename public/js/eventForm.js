88c769feca51690b5f7255a3c5ed1eb5fc5c5b85
tlentz <tyler.cg.lentz@gmail.com>
2017-04-11 11:46:51 -0500

    update grand total correctly with qty

--- public/js/eventForm.js
+++ public/js/eventForm.js
@@ -1,18 +1,13 @@
 $( document ).ready(function() {
+
+	// add datepicker to all .datepicker classes
 	$(".date").datepicker();
 
-	// Display "add inventory" modal.
-	$(".add-btn").on('click', function() {
-			var tblID = $(this).next('table').attr('id');
-			$('#modal-table-id').val(tblID);
-			$("#mdlAddItem").modal();
-	});
-
-	// Load price on item select
+	// Load price in modal on item select
 	$("#altar-flowers").on('change', function()	{
 		if ($(this).prop('selectedIndex') != 0)	{
 			var name = $("option:selected", this).text();
-			var price = prices[name];
+			var price = parseFloat(prices[name]).toFixed(2);
 			$(this).siblings('#addPrice').val(price);
 			$(this).siblings('#addQuantity').val(1);
 		}
@@ -21,55 +16,71 @@ $( document ).ready(function() {
 			$(this).siblings('#addQuantity').val(null);
 		}
 	});
+
 });
 
-	// Add new item to recipe table
-	$("#addItem").on('click', function()	{
-		if ($("#altar-flowers").prop("selectedIndex") != 0)	{
-			event.preventDefault();
+// Display "add inventory" modal.
+$(document).on("click", ".add-btn", function() {
+		var tblID = $(this).next('table').attr('id');
+		$('#modal-table-id').val(tblID);
+		$("#mdlAddItem").modal();
+});
 
-			var $form = $(this).closest('form');
-			var item = $form.find('#altar-flowers option:selected').text();
-			var qty = $form.find('#addQuantity').val();
-			var price = $form.find('#addPrice').val();
-			var tblID = $form.find('#modal-table-id').val();
-			var tbl = $("#" + tblID);
-			tbl.prepend("<tr><td>" + item + "</td><td style='text-align: right;'>" + qty + " x </td><td>$" + price + "</td><td><center><input type='button' class='remove-item-btn' value='x' /></ center></td></tr>");
-			var qtyPrice = parseFloat(qty) * parseFloat(price);
-			var tblTotal = tbl.find('.price');
-			var initPrice = tblTotal.text();
-			initPrice = initPrice.replace(/[,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-			totalToDisp = parseFloat(initPrice) + parseFloat(qtyPrice);
-			tblTotal.html("<b>$" + totalToDisp.toFixed(2) + "</b>");
-			$("#mdlAddItem").modal('hide');
-		}
-		else {
-			event.preventDefault();
-			alert("Please select a valid item.");
-		}
-	});
+// Load price in modal on item select
+$(document).on("change", ".altar-flowers", function() {
+	if ($(this).prop('selectedIndex') != 0)	{
+		var name = $("option:selected", this).text();
+		var price = parseFloat(prices[name]).toFixed(2);
+		$(this).siblings('#addPrice').val(price);
+		$(this).siblings('#addQuantity').val(1);
+	}
+	else {
+		$(this).siblings('#addPrice').val(price);
+		$(this).siblings('#addQuantity').val(null);
+	}
+});
 
-	// Remove item from recipe table
-	$(document).on("click", ".remove-item-btn", function ()	{
+// Add new item to recipe table
+$(document).on("click", "#addItem", function() {
+	if ($("#altar-flowers").prop("selectedIndex") != 0)	{
 		event.preventDefault();
-		var parentTbl = $(this).closest('table');
-		var rowClicked = $(this).closest('tr');
-		var initQty = $(this).closest('td').prev('td').prev('td').text();
-		initQty = initQty.substr(0, 1);
-		initQty = parseFloat(initQty);
-		var pricePerItem = $(this).closest('td').prev('td').text();
-		pricePerItem = pricePerItem.replace(/[,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-		pricePerItem = parseFloat(pricePerItem);
-		var currTblTotal = parentTbl.find('.price').text();
-		currTblTotal = currTblTotal.replace(/[,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-		currTblTotal = parseFloat(currTblTotal);
-		var newTotal = currTblTotal - pricePerItem;
-		parentTbl.find('.price').html('<b>$' + newTotal.toFixed(2) + '</b>');
-		if (initQty > 1)	{
-			var newQty = initQty - 1;
-			$(this).closest('td').prev('td').prev('td').text(newQty + ' x');
+
+		var $form = $(this).closest('form');
+		var item = $form.find('#altar-flowers option:selected').text();
+		var qty = $form.find('#addQuantity').val();
+		var price = $form.find('#addPrice').val();
+		price = parseFloat(price);
+		var tblID = $form.find('#modal-table-id').val();
+		var tbl = $("#" + tblID);
+		tbl.prepend("<tr><td>" + item + "</td><td style='text-align: right;'>" + qty + " x </td><td>$" + price.toFixed(2) + "</td><td><center><input type='button' class='increase-item-btn' value='+' /> <input type='button' class='decrease-item-btn' value='-' /> <input type='button' class='remove-item-btn' value='x' /></ center></td></tr>");
+		var qtyPrice = parseFloat(qty) * parseFloat(price);
+		var tblTotal = tbl.find('.price');
+		var initPrice = tblTotal.text();
+		initPrice = initPrice.replace(/[,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+		totalToDisp = parseFloat(initPrice) + parseFloat(qtyPrice);
+		tblTotal.html("<b>$" + totalToDisp.toFixed(2) + "</b>");
+		$("#mdlAddItem").modal('hide');
+		updateGrandTotal(totalToDisp, "add")
+		var qtyTblToUpdate = tbl.closest('.itemTable');
+		var tblID2 = qtyTblToUpdate.attr("id");
+		var numRows = $("#" + tblID2 + " > tbody > tr").length;
+		qtyVal = qtyTblToUpdate.parent().next().children('.form-control-num').val();
+		if (qtyVal == null | qtyVal == "" | qtyVal == 0)
+		{
+			qtyTblToUpdate.parent().next().children('.form-control-num').defaultValue = 1;
+			qtyTblToUpdate.parent().next().children('.form-control-num').val(1);
 		}
-		else {
-			rowClicked.remove();
-		}
-	});
+	}
+	else {
+		event.preventDefault();
+		alert("Please select a valid item.");
+	}
+});
+
+// Check if value of input isn't set
+function isEmpty(str) {
+	if(str == "TBA") {
+		return true;
+	}
+	return (!str || /^\s*$/.test(str));
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,12 @@
-$( document ).ready(function() {
-
-  $(".currency").on('change', function() {
-    // no characters can be typed
-    // price is always 2 decminal points
-    var sanitized = parseFloat($(this).val().replace(/,/g, ""))
-                    .toFixed(2);
-                    //.toString()
-                    //.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    $(this).val(sanitized);
-  });
+// format currency on keyup
+$(document).on("keyup", ".currency", function ()	{
+  // no characters can be typed
+  // price is always 2 decminal points
+  var sanitized = parseFloat($(this).val().replace(/,/g, "")).toFixed(2);
+  $(this).val(sanitized);
 });
 
+// scroll page to location of tags
 var navigationFn = {
     goToSection: function(id) {
         $('html, body').animate({
