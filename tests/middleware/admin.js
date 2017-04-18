f44fd53649bc8e6e89ce668998e6f61858e39996
Arun P <arunparamanathan@ksu.edu>
2017-04-18 10:20:51 -0500

    adding admin tests
    adding admin tests

--- tests/middleware/admin.js
+++ tests/middleware/admin.js
@@ -1,15 +1,26 @@
 var expect = require('chai').expect;
 var admin = require('../../middleware/admin.js');
 
-describe("Middleware -- Admin Tests", function() {
+// Admin.
+describe("Middleware - Admin", function() {
 
-  it("Get Inventory Types", function() {
-    var inventoryTypes = null;
+  // Get inventory types.
+  it("- Get inventory types", function() {
+    var r = null;
     admin.getInventoryTypes(function(err, rows) {
       expect(err).to.equal(null);
-      inventoryTypes = rows;
+      r = rows;
+      expect(r).to.not.equal(null);
     });
-    expect(inventoryTypes).to.not.equal(null);
   });
 
+  // Get markups.
+  it("- Get markups", function() {
+    var r = null;
+    admin.getMarkups(function(err, rows) {
+      expect(err).to.equal(null);
+      r = rows;
+      expect(r).to.not.equal(null);
+    });
+  });
 });
