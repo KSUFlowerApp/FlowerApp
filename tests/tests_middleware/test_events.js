e59f2b018d01bb49231afb1afb21ec68defff772
Arun P <arunparamanathan@ksu.edu>
2017-04-23 11:48:15 -0500

    added events test
    added events test

--- tests/tests_middleware/test_events.js
+++ tests/tests_middleware/test_events.js
@@ -1,53 +1,13 @@
 var expect = require('chai').expect;
-var admin = require('../../middleware/admin.js');
+var admin = require('../../middleware/events.js');
 
-// Admin.
-describe("Middleware - Admin", function() {
+// Events
+describe("Middleware - Events", function() {
 
-  // Get inventory types.
-  it("- Get inventory types", function() {
+  // Get events.
+  it("- Get events", function() {
     var r = null;
-    admin.getInventoryTypes(function(err, rows) {
-      expect(err).to.equal(null);
-      r = rows;
-      expect(r).to.not.equal(null);
-    });
-  });
-
-  // Get markups.
-  it("- Get markups", function() {
-    var r = null;
-    admin.getMarkups(function(err, rows) {
-      expect(err).to.equal(null);
-      r = rows;
-      expect(r).to.not.equal(null);
-    });
-  });
-
-  // Get users.
-  it("- Get users", function() {
-    var r = null;
-    admin.getUsers(function(err, rows) {
-      expect(err).to.equal(null);
-      r = rows;
-      expect(r).to.not.equal(null);
-    });
-  });
-
-  // Get roles.
-  it("- Get roles", function() {
-    var r = null;
-    admin.getRoles(function(err, rows) {
-      expect(err).to.equal(null);
-      r = rows;
-      expect(r).to.not.equal(null);
-    });
-  });
-
-  // Get taxes.
-  it("- Get taxes", function() {
-    var r = null;
-    admin.getTaxes(function(err, rows) {
+    admin.getEvents(function(err, rows) {
       expect(err).to.equal(null);
       r = rows;
       expect(r).to.not.equal(null);
